4ba48897957d2153a6b852bf1f68b495bfaebdc1
Jack Cheng <randomno@hotmail.com>
2021-06-14 01:00:12 +0800

    temp

--- all.js
+++ all.js
@@ -1,42 +1,86 @@
+Object.keys(VeeValidateRules).forEach(rule => {
+    if (rule !== 'default') {
+        VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+    }
+});
+VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+
+// Activate the locale
+VeeValidate.configure({
+    generateMessage: VeeValidateI18n.localize('zh_TW'),
+    validateOnInput: true, // 調整為輸入字元立即進行驗證
+});
+
+const url = 'https://vue3-course-api.hexschool.io';
+const apiPath = 'randomno';
+
 import pagination from './pagination.js';
+import productModal from './productModal.js';
 
 const app = Vue.createApp({
     components: {
-        pagination
+        pagination,
+        productModal
     },
-    data(){
-        return{
-            url: 'https://vue3-course-api.hexschool.io',
-            apiPath: 'randomno',
+    data() {
+        return {
+            productInfo: [],
             products: [],
-            purchase: []
+            purchase: [],
+            pagination
         }
     },
-    methods:{
-        loading(){
-            const loader = this.$loading.show();
-            setTimeout(()=>{
-                loader.hide();
-            },1500)
+    methods: {
+        openModal() {
+            this.$refs.productModal.openModal();
         },
-        getData(page = 1){
-            axios.get(`${this.url}/api/${this.apiPath}/products?page=${page}`)
-            .then(res=>{
-                if(res.data.success){
-                    const {products} = res.data;
-                    console.log(products);
-                    products.forEach(item=>{
-                        this.products.push(item)
-                    })
-                    console.log(this.products);
-                }
-            })
-            .catch(err=>console.log(err))
-        }
+        loading() {
+            const loader = this.$loading.show();
+            setTimeout(() => {
+                loader.hide();
+            }, 1500)
+        },
+        getData(page = 1) {
+            axios.get(`${url}/api/${apiPath}/products?page=${page}`)
+                .then(res => {
+                    if (res.data.success) {
+                        this.products = res.data.products;
+                        this.pagination = res.data.pagination;
+                    }
+                })
+                .catch(err => console.log(err))
+        },
+        getInfo(id) {
+            axios.get(`${url}/api/${apiPath}/product/${id}`)
+                .then(res => {
+                    this.productInfo = res.data.product;
+                    this.openModal();
+                })
+                .catch(err => console.log(err))
+        },
+        addCart(id, qty) {
+            axios.post(`${url}/api/${apiPath}/cart`, { "data": { "product_id": id, "qty": qty } })
+                .then(res => {
+                    this.updateCart();
+                })
+                .catch(err => console.log(err))
+        },
+        updateCart() {
+            axios.get(`${url}/api/${apiPath}/cart`)
+                .then(res => {
+                    this.purchase = res.data.data.carts;
+                    console.log(res.data.data.carts);
+                })
+                .catch(err => { console.log(err) })
+        },
     },
-    mounted(){
+    mounted() {
+        this.getData();
+        this.updateCart();
     }
 })
-
+app.component('VForm', VeeValidate.Form);
+app.component('VField', VeeValidate.Field);
+app.component('ErrorMessage', VeeValidate.ErrorMessage);
 app.use(VueLoading);
 app.mount('#app');
--- pagination.js
+++ pagination.js
@@ -1,13 +1,17 @@
 export default {
-    props: ['page'],
-    template: `<nav aria-label="Page navigation example">
+  props: ['page'],
+  template: `<nav aria-label="Page navigation example">
     <ul class="pagination d-flex justify-content-center small">
-      <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item"><a class="page-link" href="#">Next</a></li>
+      <li class="page-item" :class="{'disabled ': !page.has_pre}">
+        <a class="page-link" href="#" @click="$emit('get-data', page.current_page-1)">Previous</a>
+      </li>
+      <li class="page-item" :class="{'active':item === page.current_page}"
+        v-for="item in page.total_pages" :key="item">
+        <a class="page-link" href="#" @click="$emit('get-data', item)">{{item}}</a>
+      </li>
+      <li class="page-item" :class="{'disabled ': !page.has_next  }">
+        <a class="page-link" href="#" @click="$emit('get-data', page.current_page+1)">Next</a>
+      </li>
     </ul>
-  </nav>`,
-    mounted() { }
+  </nav>`
 }
